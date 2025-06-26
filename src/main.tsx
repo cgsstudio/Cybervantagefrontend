210c4ba17832dfa7b2366c7fa498fc3b4d473f8b
dhirahuja <ahujadhir@gmail.com>
2025-06-26 17:56:41 +0530

    v1.6

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import './index.css';
@@ -10,9 +9,9 @@ const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
 
 createRoot(rootElement).render(
- <BrowserRouter basename="/Cybervantage">
-  <App />
-</BrowserRouter>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
 
 
